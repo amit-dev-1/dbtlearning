de8a54d192cfdccd9816c9c39738a1b929940785
AMIT YADAV <amit.yadav182k@gmail.com>
2025-09-05 05:25:22 +0000

    commit

--- models/example/gold/fact_orders_delete_insert.sql
+++ models/example/gold/fact_orders_delete_insert.sql
@@ -1,30 +1,32 @@
 {{
     config(
-        materialized='incremental',
-        is_transient=False,
-        database='ECOMMERCE_DBS',
-        schema='gold',
-        incremental_strategy='delete+insert',
-        unique_key='ord_id'
+        materialized="incremental",
+        transient=False,
+        database="ECOMMERCE_DBS",
+        schema="gold",
+        incremental_strategy="delete+insert",
+        unique_key="ord_id",
     )
 }}
-with joined as
-(
-    select
-        o.ord_id,
-        o.cust_id,
-        o.purchase_qty,
-        o.TOT_AMT_SPEND,
-        o.tot_discount,
-        c.customer_bank_balance,
-        o.order_date as order_date
-    from {{ref("orders_cleaned")}} o left join {{ref("customer_cleaned")}} c on o.cust_id=c.customer_id
-),
-filtered as 
-(
-    select * from joined
-    {% if is_incremental() %}
-    where order_date >= dateadd('day',-2,current_timestamp())
-    {% endif %}
-)
-select * from filtered
+with
+    joined as (
+        select
+            o.ord_id,
+            o.cust_id,
+            o.purchase_qty,
+            o.tot_amt_spend,
+            o.tot_discount,
+            c.customer_bank_balance,
+            o.order_date as order_date
+        from {{ ref("orders_cleaned") }} o
+        left join {{ ref("customer_cleaned") }} c on o.cust_id = c.customer_id
+    ),
+    filtered as (
+        select *
+        from joined
+        {% if is_incremental() %}
+            where order_date >= dateadd('day', -2, current_timestamp())
+        {% endif %}
+    )
+select *
+from filtered
